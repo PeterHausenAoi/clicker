d9e2dfe61b3c87f774da3f4e2a64fc6da7b169a4
Gergely Váraljai <varaljai.gergely@gmail.com>
2023-01-08 21:54:44 +0100

    deploy updated

--- deploy.sh
+++ deploy.sh
@@ -21,14 +21,7 @@ cd clicker-main
 echo "Packaged"
 
 echo "Deploying..."
-echo "Copy jar..."
 sudo cp target/Clicker-0.0.1-SNAPSHOT.jar ../../bin/clicker.jar
-
-cd /app
-
-echo "Copy service..."
-sudo cp tmp/clicker-main/clicker.service /etc/systemd/system
-rm -rf tmp
 
 echo "Setting env vars..."
 
@@ -39,10 +32,25 @@ export RDS_PORT=$(aws ssm get-parameter --name="/clicker/RDS_PORT" --query "Para
 export RDS_USER=$(aws ssm get-parameter --name="/clicker/RDS_USER" --query "Parameter.Value" --output text)
 export RDS_PASSWORD=$(aws ssm get-parameter --name="/clicker/RDS_PASSWORD" --query "Parameter.Value" --output text)
 
+sed -i "s/RDS_ENDPOINT_PLACEHOLDER/$RDS_ENDPOINT/" clicker.conf
+sed -i "s/RDS_PORT_PLACEHOLDER/$RDS_PORT/" clicker.conf
+sed -i "s/RDS_USER_PLACEHOLDER/$RDS_USER/" clicker.conf
+sed -i "s/RDS_PASSWORD_PLACEHOLDER/$RDS_PASSWORD/" clicker.conf
+sed -i "s/AWS_DEFAULT_REGION_PLACEHOLDER/$AWS_DEFAULT_REGION/" clicker.conf
+
+sudo cp clicker.conf /etc/systemd/system
+sudo cat /etc/systemd/system/clicker.conf
+
 echo "Env vars set."
 
+cd /app
+
+echo "Copy service..."
+sudo cp tmp/clicker-main/clicker.service /etc/systemd/system
+rm -rf tmp
+
 echo "Start service..."
-sudo systemctl start clicker.service
+sudo systemctl restart clicker.service
 sudo systemctl enable clicker.service
 sudo systemctl daemon-reload
 sudo systemctl status clicker.service
